689405ec536409618a1b1ba04a2d0e9529cb437d
Alknight17 <knightalex17@gmail.com>
2022-07-21 17:37:31 -0400

    set up basic functionality of page changing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
 
 function App() {
+  const [ page, setPage ] = useState('About Me');
+  const checkPage = () => {
+    if (page === 'About Me'){
+      return <AboutMe /> 
+    } else if (page === 'Contact') {
+      return <Contact />
+    } else if (page === 'Resume') {
+      return <Resume />
+    } else if (page === 'Portfolio') {
+      return <Portfolio />
+    } else { return <h1>Something went wrong!</h1>}
+  }; 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {checkPage()}
+      <Footer />
     </div>
   );
 }
