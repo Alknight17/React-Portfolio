a5a8da032c9d6be5e166166feb14ff309870a17d
Alknight17 <knightalex17@gmail.com>
2022-07-27 15:23:20 -0400

    setup layout/Navbar folders and files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import './App.scss';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Contact from './components/Contact';
@@ -7,26 +6,40 @@ import Footer from './components/Footer';
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
 
+import './App.scss';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+
+// function App() {
+//   const [ page, setPage ] = useState('About Me');
+//   const checkPage = () => {
+//     if (page === 'About Me'){
+//       return <AboutMe /> 
+//     } else if (page === 'Contact') {
+//       return <Contact />
+//     } else if (page === 'Resume') {
+//       return <Resume />
+//     } else if (page === 'Portfolio') {
+//       return <Portfolio />
+//     } else { return <h1>Something went wrong!</h1>}
+//   }; 
+//   return (
+//     <div className="App">
+//       <Header setPage={setPage} />
+//       {checkPage()}
+//       <Footer />
+//     </div>
+//   );
+// }
+
 function App() {
-  const [ page, setPage ] = useState('About Me');
-  const checkPage = () => {
-    if (page === 'About Me'){
-      return <AboutMe /> 
-    } else if (page === 'Contact') {
-      return <Contact />
-    } else if (page === 'Resume') {
-      return <Resume />
-    } else if (page === 'Portfolio') {
-      return <Portfolio />
-    } else { return <h1>Something went wrong!</h1>}
-  }; 
   return (
-    <div className="App">
-      <Header setPage={setPage} />
-      {checkPage()}
-      <Footer />
-    </div>
-  );
+    <>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+    </Routes>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+
+
+
+const Navbar = () => (
+    <div className='nav-bar'>
+        
+    </div>
+)
+
+export default Navbar;
