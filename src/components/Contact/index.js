5b558b314041425cb3baf46083bcc3bf397983fb
Alknight17 <knightalex17@gmail.com>
2022-07-28 17:18:50 -0400

    setup Contact layout/styling, needs work

---
+++ src/components/Contact/index.js
@@ -0,0 +1,45 @@
+import "./index.scss";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>Contact Me</h1>
+          <p>Insert placeholder paragraph that will eventually say something</p>
+          <br></br>
+        </div>
+        <div className="contact-form">
+          <form>
+            <ul>
+              <li className="half">
+                <input type="text" name="name" placeholder="Name" required />
+              </li>
+              <li className="half">
+                <input type="email" name="email" placeholder="Email" required />
+              </li>
+              <li>
+                <input
+                  placeholder="Subject"
+                  type="text"
+                  name="Subject"
+                  required
+                />
+              </li>
+              <li>
+                <textarea placeholder="Message" name="message" required></textarea>
+              </li>
+              <li>
+                <input type="submit" className="flat-button" value="SEND" />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+
+
+export default Contact;
