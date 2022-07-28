c75d413ab2af2b232201013b03772499964d959e
Alknight17 <knightalex17@gmail.com>
2022-07-28 12:43:02 -0400

    Create Navbar links/icons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,32 @@
-import './index.scss'
-
-
+import "./index.scss";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => (
-    <div className='nav-bar'>
-        
-    </div>
-)
+  <div className="nav-bar">
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+  </div>
+);
 
 export default Navbar;
